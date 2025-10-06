7f44c3b3fe7c01b1cedf242d88891fcaf35a9dda
atharvabijalwan <atharva.bijalwan@gmail.com>
2025-10-06 03:07:25 -0600

    Add files via upload

---
+++ git_day1_pr2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+    int a , b ;
+
+    printf("enter the value of first number a  \n ") ;
+    scanf("%d" , &a) ;
+
+    printf("enter the value of second number b  \n ") ;
+    scanf("%d" , &b) ;
+
+    printf("the sum of a and b is %d \n " , a + b) ;
+    printf("the product  of a and b is %d \n " , a*b) ;
+    printf("the difference of a and b is %d \n " , a-b) ;
+    printf("the quatient of a and b is %d \n " , a/b) ;
+}
