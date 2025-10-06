4c9f247192b969d39d6ea3f2544ef6fa88213507
atharvabijalwan <atharva.bijalwan@gmail.com>
2025-10-06 11:21:00 -0600

    Add files via upload

---
+++ github_day6_pr1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main (){
+
+    int a ;
+    printf(" enter the positive number \n");
+    scanf("%d", &a);
+
+    if (a%2==0)
+    {
+        printf("the given number is even \n");
+    }
+     else
+     {
+        printf("the given number is an odd number \n");
+
+     }
+
+     return 0;
+}
