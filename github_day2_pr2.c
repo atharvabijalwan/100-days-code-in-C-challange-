018d8a9b1b9c3f37b333eac6217f98aa4aba709d
atharvabijalwan <atharva.bijalwan@gmail.com>
2025-10-06 05:15:19 -0600

    Add files via upload

---
+++ github_day2_pr2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+    
+    int area , radius =6 , circumference ;
+
+    circumference = 2 * 3.14 * radius;
+    area = 3.14 * radius * radius;
+
+    printf("Circumference: %d\n", circumference);
+    printf("Area: %d\n", area);
+
+   
+
+    return 0;
+}
